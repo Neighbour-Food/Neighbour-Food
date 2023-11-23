b84526eb15d726bccf584e7a48cf38aea7e3da96
Michael-Weckop <michaelweckop@gmail.com>
2023-11-22 22:40:20 -0500

    intial typescrypt and next.js setup

---
+++ next.config.js
@@ -0,0 +1,20 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.mdx/,
+      use: [
+        options.defaultLoaders.babel,
+        {
+          loader: '@mdx-js/loader',
+        },
+      ],
+    })
+ 
+    return config
+  },
+}
+ 
+export default nextConfig
