ae5410d2d58d24b2e14b98a3ed005f74e7ca6444
Renee Toscan <renee.toscan@outlook.com>
2023-11-25 11:32:17 -0800

    files and routes created

---
+++ client/components/Restaurants/CreatePickup.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from "react";
+
+const CreatePickup: FC = () => {
+  return (
+    <>
+      <h1>Create Pickup for Restaurants</h1>
+    </>
+  );
+};
+
+export default CreatePickup;
