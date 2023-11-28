6536afe2ccf11a77bbaf9cfde57684731e0c2a3d
Renee Toscan <renee.toscan@outlook.com>
2023-11-28 08:11:58 -0800

    set up redux

--- client/components/Auth/Signup.tsx
+++ client/components/Auth/Signup.tsx
@@ -1,10 +1,31 @@
-import React, { FC } from "react";
+import React, { ChangeEvent, FC, ReactHTMLElement } from "react";
+import { useSelector, useDispatch } from "react-redux/";
+import { RootState } from "../../state/store";
+import { changeIsSignedIn, changeUserName, changeNameToAction } from "../../state/user/userSlice";
 
 const Signup: FC = () => {
+  const isSignedIn = useSelector((state: RootState) => state.user.isSignedIn);
+  const userName = useSelector((state: RootState) => state.user.userName);
+  
+  const dispatch = useDispatch();
+
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newName = event.target.value;
+    dispatch(changeNameToAction(newName));
+  };
   return (
-    <>
-      <h1>Signup Page</h1>
-    </>
+    <div>
+      <h1>{userName}</h1>
+      <div>
+        <button onClick={() => dispatch(changeIsSignedIn())}>
+          change isSignedIn
+        </button>
+        <button onClick={() => dispatch(changeNameToAction('Julia'))}>
+          change username
+        </button>
+        <input onChange={handleNameChange}></input>
+      </div>
+    </div>
   );
 };
 
