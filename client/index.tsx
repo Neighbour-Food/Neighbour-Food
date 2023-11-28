a949ae6469c0000172df625efacf24f1cb9bce72
Michael-Weckop <michaelweckop@gmail.com>
2023-11-28 16:25:06 -0500

    added signup component

--- client/index.tsx
+++ client/index.tsx
@@ -3,9 +3,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./state/store";
+import './body.css'
+import './components/Auth/auth.css'
 
 createRoot(document.querySelector("#root")).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+    <Provider store={store}>
+        <App />
+    </Provider>
 );
