60e9c6ba4a50624b8ce860ffee32d3d972c7fafa
Renee Toscan <renee.toscan@outlook.com>
2023-11-29 18:45:37 -0800

    merged with rt/npo

---
+++ assets/downArrow.tsx
@@ -0,0 +1,35 @@
+import React, { FC } from "react";
+
+const npoFeedImage: FC = () => {
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+      >
+        <mask
+          id="mask0_39_542"
+          // style="mask-type:alpha"
+          maskUnits="userSpaceOnUse"
+          x="0"
+          y="0"
+          width="24"
+          height="24"
+        >
+          <rect width="24" height="24" fill="#D9D9D9" />
+        </mask>
+        <g mask="url(#mask0_39_542)">
+          <path
+            d="M12.0001 14.7019L6.69238 9.39422L7.40008 8.68652L12.0001 13.2865L16.6001 8.68652L17.3078 9.39422L12.0001 14.7019Z"
+            fill="#12223E"
+          />
+        </g>
+      </svg>
+    </>
+  );
+};
+
+export default npoFeedImage;
--- client/components/NPOs/Feed.tsx
+++ client/components/NPOs/Feed.tsx
@@ -1,9 +1,33 @@
 import React, { FC } from "react";
+import { useSelector } from "react-redux/";
+import { RootState } from "../../state/store";
+import NpoFeedImage from "../../../assets/npoFeedImage";
+import OrdersList from "./OrdersList";
+import PersonIcon from "../../../assets/personIcon";
+import Sidebar from "../Sidebar";
+import "./feed.css";
 
 const Feed: FC = () => {
+  const orgName = useSelector((state: RootState) => state.user.orgName);
+
   return (
     <>
-      <h1>Feed for NPOs</h1>
+      <div className="feed">
+        <Sidebar />
+        <div className="npo-feed">
+          {/* <nav>
+          <button type="button">
+          <PersonIcon />
+          </button>
+        </nav> */}
+          <h1>
+            Hi {orgName},<br />
+            <span> 3 restaurants</span> are ready for pick up
+          </h1>
+          <NpoFeedImage />
+          <OrdersList />
+        </div>
+      </div>
     </>
   );
 };
