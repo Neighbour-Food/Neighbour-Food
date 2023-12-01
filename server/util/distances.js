fe80172069106a85e66c17730e978f11366426a1
Liam <lhodges3@binghamton.edu>
2023-11-30 22:47:37 -0500

    updated available meals route

--- server/util/distances.js
+++ server/util/distances.js
@@ -3,6 +3,8 @@
 const distanceDifference = (origin, restOptions) => {
     // assume maybe we just send an array of the resturaunts in the same state and same type (vegan, veg, etc.) that have meals to offer?
 
+    // {lng: , lat: } [{rest_id: , lng , lat}, ...]
+
     // origin to be object with lat, long and distance pref of NPO
     // rest options to be an array with objects, containign rest id, long and lat,
     
@@ -16,11 +18,9 @@ const distanceDifference = (origin, restOptions) => {
     const earthRadius = 6371; // km
 
     restOptions.forEach(rest => {
-        const dLat = degreesToRadians(origin.lat - rest.lat);
-        const dLng = degreesToRadians(origin.lng - rest.lng);
-        
-        const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(degreesToRadians(rest.lat)) * Math.sin(dLng / 2) * Math.sin(dLng / 2);
-        
+        const dLat = degreesToRadians(origin.latitude - rest.latitude);
+        const dLng = degreesToRadians(origin.longitude - rest.longitude);
+        const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(degreesToRadians(rest.latitude)) * Math.sin(dLng / 2) * Math.sin(dLng / 2);
         const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
         const distanceKm = earthRadius * c;
         const distanceMi = distanceKm / 1.609; //convert km to mi
